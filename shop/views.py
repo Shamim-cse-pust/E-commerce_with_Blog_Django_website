5e409ea9128346cd49bd4703196969cbbe0a4aef
Shamim-cse-pust <shamim.pust.cse@gmail.com>
2024-06-29 02:02:54 +0600

    create navbar and card with static directory for image

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request,"shop/index.html")
